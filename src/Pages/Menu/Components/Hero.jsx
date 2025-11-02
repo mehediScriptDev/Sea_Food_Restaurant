b9895b94d64b3e5a9045687094d34eca95709d83
mehediScriptDev <mehedi25122003@gmail.com>
2025-11-02 21:08:23 +0600

    feat: Menu section started

---
+++ src/Pages/Menu/Components/Hero.jsx
@@ -0,0 +1,11 @@
+import banner from '../../../../assets/menu/banner3.jpg'
+
+const Hero = () => {
+    return (
+        <div style={{ backgroundImage: `url(${banner})` }} className='h-full bg-center bg-cover flex items-center justify-center mb-10 py-10'>
+            
+        </div>
+    );
+};
+
+export default Hero;
---
+++ src/Pages/Menu/Menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hero from './Components/Hero';
+
+const Menu = () => {
+    return (
+        <div className='container  min-h-screen'>
+            <Hero/>
+        </div>
+    );
+};
+
+export default Menu;
