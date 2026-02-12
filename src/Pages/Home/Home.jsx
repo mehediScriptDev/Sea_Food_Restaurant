4d5ff94a016fe77a8d03a6f0c3540e7ef6af78b0
mehediScriptDev <mehedi25122003@gmail.com>
2026-02-12 19:00:36 +0600

    testimonial imported

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import Banner from './Components/Banner/Banner';
-import Category from './Components/Categories/Category';
-import Menu from './Components/Menu/Menu';
-import ChefRecom from './Components/ChefRecom/ChefRecom';
-import FeatureMenu from './Components/FeatureMenu/FeatureMenu';
-import Testimonial from './Components/Testimonial/Testimonial';
+import React from "react";
+import Banner from "./Components/Banner/Banner";
+import Category from "./Components/Categories/Category";
+import Menu from "./Components/Menu/Menu";
+import ChefRecom from "./Components/ChefRecom/ChefRecom";
+import FeatureMenu from "./Components/FeatureMenu/FeatureMenu";
+
+import Testimonial from "./Components/Testimonial/Testimonial";
 
 const Home = () => {
-    return (
-        <div className=''>
-            <Banner/>
-            <Category/>
-            <Menu/>
-            <ChefRecom/>
-            <FeatureMenu/>
-            <Testimonial/>
-        </div>
-    );
+  return (
+    <div className="">
+      <Banner />
+      <Category />
+      <Menu />
+      <ChefRecom />
+      <FeatureMenu />
+      <Testimonial />
+    </div>
+  );
 };
 
 export default Home;
